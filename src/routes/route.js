61e7cbd6e376aec76fab83ba32621dcc54e86a80
akthinq1 <akthinq@gmail.com>
2022-06-04 20:19:45 +0530

    myapi

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,41 +1,105 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
+
+/////////////////////////////////////////////////////////
+
+//Question1
+
+router.get('/movies', function (req, res) {
+
+    let movies = 
+    ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+
+    res.send(movies)
 });
 
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
+/////////////////////////////////////////////////////////
+
+//Question2
+
+router.get('/movies/:indexNumber', function (req, res) {
+
+    const value = req.params.index;
+    let movies = 
+    ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+
+    res.send(movies[value])
 });
 
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-})
+//////////////////////////////////////////////////////////////
 
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
-})
+// Question3
+
+router.get('/movies2/:indexNumber', function (req, res){
+
+    const valu = req.params.index;
+    let movies = ['Rang de basanti', 'The shining', 'Lord of the rings',  'Batman begins']
+    let displayMoive
+    if(valu < movies.length){
+        res.send(movies[valu])
+    }else{
+        res.send("use a valid index number")
+    }
+    
+});
+
+//////////////////////////////////////////////////////////////////
+
+//Question4
+
+router.get('/films', function(req,res){
+    const films =  [ {
+          "id": 1,
+          "name": "The Shining"
+         }, {
+          "id": 2,
+          "name": "Incendies"
+         }, {
+          "id": 3,
+          "name": "Rang de Basanti"
+         }, {
+          "id": 4,
+          "name": "Finding Nemo"
+         }]
+      res.send(films)      
+  });
+
+  ////////////////////////////////////////////////////////////////
+
+  //Question.5
+
+  router.get('/films/:filmId', function(req,res){
+    const films =  [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+    let displayingFilm
+
+    if(req.params.filmId <= films.length && req.params.filmId != 0){
+        for(index = 0; index< films.length; index++){
+            if(req.params.filmId == films[index].id){
+                displayingFilm = films[index].name
+                break
+            }
+        }
+    }else{
+        displayingFilm = "no film at this index"
+    }
+
+    res.send(displayingFilm)
+});
+
 
 
 module.exports = router;
-// adding this comment for no reason
